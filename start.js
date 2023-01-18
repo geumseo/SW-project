3d6d02c5976210b8f08c74870dd7a860c858ccb3
LSUHYEON <103477098+LSUHYEON@users.noreply.github.com>
2023-01-19 01:37:13 +0900

    Update start.js

--- start.js
+++ start.js
@@ -21,6 +21,146 @@ function setResult(){
   var resultImg = document.createElement('img');
   const imgDiv = document.querySelector('#resultImg');
   var imgURL = 'img/' + point + '.jpg';
+  resultImg.src = imgURL;
+  resultImg.alt = point;
+  resultImg.classList.add('img-fluid');
+  imgDiv.appendChild(resultImg);
+
+  const resultDesc = document.querySelector('.resultDesc');
+  resultDesc.innerHTML = infoList[point].desc;
+}
+
+function goResult(){
+  qna.style.WebkitAnimation = "fadeOut 1s";
+  qna.style.animation = "fadeOut 1s";
+  setTimeout(() => {
+    result.style.WebkitAnimation = "fadeIn 1s";
+    result.style.animation = "fadeIn 1s";
+    setTimeout(() => {
+      qna.style.display = "none";//안보이게
+      result.style.display = "block" //아래에 보이게
+    }, 450)})
+    setResult();
+}
+
+function addAnswer(answerText, qIdx, idx){
+  var a = document.querySelector('.answerBox');
+  var answer = document.createElement('button');
+  answer.classList.add('answerList');
+  answer.classList.add('my-3');
+  answer.classList.add('py-3');
+  answer.classList.add('mx-auto');
+  answer.classList.add('fadeIn');
+
+  a.appendChild(answer);
+  answer.innerHTML = answerText;
+
+  answer.addEventListener("click", function(){
+    var children = document.querySelectorAll('.answerList');
+    for(let i = 0; i < children.length; i++){
+      children[i].disabled = true;
+      children[i].style.WebkitAnimation = "fadeOut 0.5s";
+      children[i].style.animation = "fadeOut 0.5s";
+    }
+    setTimeout(() => {
+      var target = qnaList[qIdx].a[idx].type;
+      for(let i = 0; i < target.length; i++){
+
+        select[target[i]] += 1;
+      
+      }
+
+      for(let i = 0; i < children.length; i++){
+        children[i].style.display = 'none';
+      }
+      goNext(++qIdx);
+    },450)
+  }, false);
+}
+
+function goNext(qIdx){
+  // 질문 인덱스가 마지막이라면 결과화면
+  if(qIdx === endPoint){ 
+    goResult();
+    return;
+  }
+
+  var q = document.querySelector('.qBox');
+  q.innerHTML = qnaList[qIdx].q;
+  for(let i in qnaList[qIdx].a){
+    addAnswer(qnaList[qIdx].a[i].answer, qIdx, i);
+  }
+  // 상태바 전체 100%에서 4개 나눈것들이 점점 채워짐
+  var status = document.querySelector('.statusBar');
+  status.style.width = (100/endPoint) * (qIdx+1) + '%';
+}
+
+function begin(){
+
+  main.style.WebkitAnimation = "fadeOut 1s";
+  main.style.animation = "fadeOut 1s";
+  setTimeout(() => {
+    qna.style.WebkitAnimation = "fadeIn 1s";
+    qna.style.animation = "fadeIn 1s";
+    setTimeout(() => {
+      main.style.display = "none";
+      qna.style.display = "block"
+    }, 450)
+    let qIdx = 0;
+    goNext(qIdx);
+  }, 450);
+}
+
+function make(){
+  location.href="make.html";
+}
+
+function map(){
+  let point = calResult();
+  if(point == 8){
+    location.href="sam.html";
+  }
+  else if(point == 1){
+    location.href="one.html";
+  }
+  else if(point == 2){
+    location.href="bibimnoddle.html";
+  }
+  else{
+      location.href="map.html";
+  }
+  
+}
+
+function moveHome(){
+  location.href="index.html";
+}
+
+
+/*
+const main = document.querySelector("#main");
+const qna = document.querySelector("#qna");
+const result = document.querySelector("#result");
+
+//질문 갯수
+const endPoint = 4;
+//음식 갯수
+const select = [ 0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0, 0, 0, 0, 0, 0,0,0,0,0,0,0,0,0];
+
+function calResult(){
+  console.log(select);
+  var result = select.indexOf(Math.max(...select));
+  return result;
+}
+
+function setResult(){
+  let point = calResult();
+  const resultName = document.querySelector('.resultname');
+  resultName.innerHTML = infoList[point].name;
+
+  var resultImg = document.createElement('img');
+  const imgDiv = document.querySelector('#resultImg');
+  var imgURL = 'img/' + point + '.jpg';
   
   resultImg.src = imgURL;
   resultImg.alt = point;
@@ -123,3 +263,5 @@ function map(){
 function moveHome(){
   location.href='index.html';
 }
+
+*/
